09bf8d75c21eca13639625d1b7b477241c6a0c5e
pneg <pneg@calmskies.xyz>
2025-07-27 15:17:34 -0500

    Add zsh-window-title

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -20,6 +20,15 @@
         name = pkgs.zsh-vi-mode.pname;
         src = pkgs.zsh-vi-mode.src;
       }
+      {
+        name = "zsh-window-title";
+        src = pkgs.fetchFromGitHub {
+          owner = "olets";
+          repo = "zsh-window-title";
+          rev = "v1.2.0";
+          sha256 = "sha256-RqJmb+XYK35o+FjUyqGZHD6r1Ku1lmckX41aXtVIUJQ=";
+        };
+      }
     ];
 
     # zshrc
