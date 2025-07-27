5d76c51cd538263b825bada9e36c26c6ef528e5b
pneg <pneg@calmskies.xyz>
2025-07-27 15:14:43 -0500

    Add gaming packages

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -4,5 +4,36 @@
 
   programs.steam.enable = true;
   programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+    steam-tui
+    heroic
+    fluidsynth
+    (pkgs.stdenv.mkDerivation rec {
+      pname = "protonhax";
+      version = "1.0.5";
+
+      src = fetchFromGitHub {
+        owner = "jcnils";
+        repo = pname;
+        rev = version;
+        sha256 = "sha256-5G4MCWuaF/adSc9kpW/4oDWFFRpviTKMXYAuT2sFf9w=";
+      };
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp protonhax $out/bin
+      '';
+
+      postFixup = ''
+        sed -i '1s/#!/#!${
+          pkgs.lib.escape ["/"] (pkgs.lib.getExe steam-run)
+        } /' $out/bin/protonhax
+      '';
+
+    })
+  ];
 
 }
